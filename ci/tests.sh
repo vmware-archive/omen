facecb7c6e8cca2ec3dae2516e8ed604ceabf382
James Wynne <jwynne@pivotal.io>
2018-05-28 13:54:16 +0100

    Fix output dir
    [#157911292](https://www.pivotaltracker.com/story/show/157911292)
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- ci/tests.sh
+++ ci/tests.sh
@@ -4,9 +4,13 @@ set -eu
 mkdir -p "$GOPATH/src/github.com/pivotal-cloudops"
 cp -R git-omen "$GOPATH/src/github.com/pivotal-cloudops/omen"
 go get -u github.com/golang/dep/cmd/dep
+go get github.com/onsi/ginkgo/...
+
+
+OUTPUT_DIR=$(pwd)
 
 cd "$GOPATH/src/github.com/pivotal-cloudops/omen"
 
 dep ensure
 go test -v ./...
-go build -o omen-build/omen-linux64
+go build -o ${OUTPUT_DIR}/omen-build/omen-linux64
